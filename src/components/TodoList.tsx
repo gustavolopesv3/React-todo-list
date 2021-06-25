c9334fe232070750636bd4de872e1a42f7fa17d3
Gustavo Lopes <gustavo.lopesv3@gmail.com>
2021-06-25 16:21:14 -0500

    Listando TODO list

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,34 @@
+import { Todo } from '../models/Todo';
+import { TodoListItem } from './TodoListItem';
+
+export const TodoList = () => {
+  const todos: Todo[] = [
+    {
+      id: 1,
+      title: 'supermercado',
+      done: false,
+    },
+    {
+      id: 2,
+      title: 'academia',
+      done: false,
+    },
+  ];
+  return (
+    <table className="uk-table">
+      <caption>Lista de tarefas</caption>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Terefa</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {todos?.map((item) => {
+          return <TodoListItem key={item.id} todo={item}></TodoListItem>;
+        })}
+      </tbody>
+    </table>
+  );
+};
